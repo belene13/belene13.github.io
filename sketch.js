5c6ba359d76cfefd07c6e5c912dcee655562428c
belene13 <beluarevalo.1343@gmail.com>
2026-01-28 22:54:04 -0500

    cursor 3

--- sketch.js
+++ sketch.js
@@ -1,63 +1,21 @@
-let balls = [];
-const BALL_COUNT = 60;
-const THRESHOLD = 1.15;
-const SPEED = 2.2;
+const cursor = document.getElementById("cursor-follower");
 
-function setup() {
-  pixelDensity(1);
-  createCanvas(windowWidth, windowHeight);
-  noStroke();
+let mouseX = 0, mouseY = 0;
+let x = 0, y = 0;
 
-  for (let i = 0; i < BALL_COUNT; i++) {
-    balls.push({
-      x: random(width),
-      y: random(height),
-      vx: random(-SPEED, SPEED),
-      vy: random(-SPEED, SPEED),
-      r: random(16, 24)
-    });
-  }
+window.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+  x += (mouseX - x) * 0.15;
+  y += (mouseY - y) * 0.15;
+
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+
+  requestAnimationFrame(animate);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  loadPixels();
-
-  for (let i = 0; i < pixels.length; i += 4) {
-    pixels[i + 3] = 0;
-  }
-
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let sum = 0;
-
-      for (let b of balls) {
-        let dx = x - b.x;
-        let dy = y - b.y;
-        let d = sqrt(dx * dx + dy * dy) || 1;
-        sum += (b.r * b.r) / (d * d);
-      }
-
-      if (sum > THRESHOLD) {
-        let index = 4 * (y * width + x);
-        pixels[index] = 255;
-        pixels[index + 1] = 60;
-        pixels[index + 2] = 120;
-        pixels[index + 3] = 255;
-      }
-    }
-  }
-
-  updatePixels();
-
-  for (let b of balls) {
-    b.x += b.vx;
-    b.y += b.vy;
-
-    if (b.x < 0 || b.x > width) b.vx *= -1;
-    if (b.y < 0 || b.y > height) b.vy *= -1;
-  }
-}
+animate();
