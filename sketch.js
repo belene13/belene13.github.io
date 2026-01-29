be481db9b435e9dceb65c32627acc932aae2dd12
belene13 <beluarevalo.1343@gmail.com>
2026-01-28 22:05:04 -0500

    Fondo

--- sketch.js
+++ sketch.js
@@ -1,37 +1,63 @@
-let posx;
-let posy;
-let diam;
-let rad;
-
-let velx;
-let vely;
+let balls = [];
+const BALL_COUNT = 60;
+const THRESHOLD = 1.15;
+const SPEED = 2.2;
 
 function setup() {
+  pixelDensity(1);
   createCanvas(windowWidth, windowHeight);
-  diam = random(50, 100)    ;
-  rad = diam / 2;
+  noStroke();
 
-  posx = random(rad, width - rad);
-  posy = random(rad, height - rad);
+  for (let i = 0; i < BALL_COUNT; i++) {
+    balls.push({
+      x: random(width),
+      y: random(height),
+      vx: random(-SPEED, SPEED),
+      vy: random(-SPEED, SPEED),
+      r: random(16, 24)
+    });
+  }
+}
 
-  velx = random(-5, 5);
-  vely = random(-5, 5);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(150);
+  loadPixels();
 
-    if(posx > width - rad || posx < rad){
-        velx *= -1;
+  for (let i = 0; i < pixels.length; i += 4) {
+    pixels[i + 3] = 0;
+  }
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let sum = 0;
+
+      for (let b of balls) {
+        let dx = x - b.x;
+        let dy = y - b.y;
+        let d = sqrt(dx * dx + dy * dy) || 1;
+        sum += (b.r * b.r) / (d * d);
+      }
+
+      if (sum > THRESHOLD) {
+        let index = 4 * (y * width + x);
+        pixels[index] = 255;
+        pixels[index + 1] = 60;
+        pixels[index + 2] = 120;
+        pixels[index + 3] = 255;
+      }
     }
+  }
 
-    if(posy > height - rad || posy < rad){
-        vely *= -1;
-    }
+  updatePixels();
 
-    posx +=velx;
-    posy +=vely;
+  for (let b of balls) {
+    b.x += b.vx;
+    b.y += b.vy;
 
-    circle(posx, posy, diam);
-    
+    if (b.x < 0 || b.x > width) b.vx *= -1;
+    if (b.y < 0 || b.y > height) b.vy *= -1;
+  }
 }
